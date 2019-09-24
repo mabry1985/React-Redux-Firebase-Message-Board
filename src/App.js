bc0198f18ff2261fc76da8e319362b5e364e3042
Josh Mabry <mabry1985@gmail.com>
2019-09-23 21:29:35 -0700

    add form functionality and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import NavBar from './components/layout/NavBar';
-import Dashboard from './components/dashboard/Dashboard.js';
-import MessageDetails from './components/messages/MessageDetails.js';
+import Dashboard from './components/dashboard/Dashboard';
+import MessageDetails from './components/messages/MessageDetails';
 import SignIn from './auth/SignIn';
 import SignUp from './auth/SignUp';
+import CreateMessage from './components/messages/CreateMessage';
 
 class App extends React.Component {
   render() {
@@ -17,6 +18,7 @@ class App extends React.Component {
             <Route path='/message/:id' component={MessageDetails} />
             <Route path='/signin' component={SignIn} />
             <Route path='/signup' component={SignUp} />
+            <Route path='/create' component={CreateMessage} />
           </Switch>
         </div>
       </BrowserRouter>
--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const SignedInLinks = () => (
   <ul className="right">
-  <li><NavLink to='/'>New Message</NavLink></li>
+  <li><NavLink to='/create'>New Message</NavLink></li>
   <li><NavLink to='/'>Log Out</NavLink></li>
   <li><NavLink to='/'className='btn btn-floating pink lighten-1'>JM</NavLink></li>
   </ul>
